66321fd9fbfd95532516194f54be978926add060
sieder <sieder00@gmail.com>
2019-10-05 20:14:47 +0300

    Updates

---
+++ precache-manifest.e6fc23582128596ee3b9014877d5919e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8a237d4fa63f1e7b6fc6d996c6c806",
+    "url": "/SiederProgressBar/index.html"
+  },
+  {
+    "revision": "3a1c7d9f29bb58e3a0f4",
+    "url": "/SiederProgressBar/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7e213e39c0f65d46e68d",
+    "url": "/SiederProgressBar/static/js/2.12f349d8.chunk.js"
+  },
+  {
+    "revision": "3a1c7d9f29bb58e3a0f4",
+    "url": "/SiederProgressBar/static/js/main.400a5a65.chunk.js"
+  },
+  {
+    "revision": "89c648092091ee95eb56",
+    "url": "/SiederProgressBar/static/js/runtime-main.3474dcca.js"
+  }
+]);
